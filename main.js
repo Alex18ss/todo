fe1abb96e2210e2224a6dcb410a178105e404b03
Alex18 <aleka0205@gmail.com>
2023-08-22 20:11:40 +0300

    todo start

---
+++ main.js
@@ -0,0 +1,42 @@
+let addBtn = document.querySelector('.plusButton')
+let dialog = document.querySelector('.popup')
+let closeBtn = document.querySelector('.close-btn')
+let todo = document.querySelector('.todo')
+let radioBtns = document.getElementsByName('radio')
+
+function createItem(object) {
+    const item = document.createElement('div')
+    item.classList.add('item')
+    const tag = document.createElement('div')
+    tag.classList.add('tag')
+    tag.textContent = object.tag
+    const title = document.createElement('p')
+    title.textContent = object.title
+    item.append(tag, title)
+    return item
+}
+
+
+addBtn.onclick = function() {
+    dialog.classList.remove('hide')
+    dialog.classList.add('show')
+}
+
+closeBtn.onclick = function() {
+    dialog.classList.remove('show')
+    dialog.classList.add('hide')
+}
+
+
+newTask.onsubmit = function(evt) {
+    evt.preventDefault()
+    const df = {tag: '', title: ''}
+    for (let radioBtn of radioBtns) {
+        if (radioBtn.checked) {
+            df.tag = radioBtn.value
+        }
+    }
+    df.title = text.value
+    
+    todo.append(createItem(df))
+}
